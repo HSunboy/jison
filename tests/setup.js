1ba2ba88c3101c489bd0ff483d230cfe1ab71e97
Ger Hobbelt <ger@hobbelt.com>
2020-11-24 19:14:22 +0100

    lingering ES5 DROP cleanups

--- tests/setup.js
+++ tests/setup.js
@@ -1,5 +1,5 @@
 
-//const Jison = require("../dist/jison-cjs-es5");
+//const Jison = require("../dist/jison-cjs");
 const Jison = require('../dist/jison-cjs');
 //exports.Jison = require("../dist/jison").Jison;
 //exports.Lexer = exports.RegExpLexer = require("../lib/util/regexp-lexer");
