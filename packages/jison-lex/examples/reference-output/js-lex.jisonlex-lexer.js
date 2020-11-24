2ef79563100dbe56d0e9ac5c8f2c13d119ef4002
Ger Hobbelt <ger@hobbelt.com>
2020-11-24 19:34:57 +0100

    adding preliminary reference + output files for the lexer tests

---
+++ packages/jison-lex/examples/reference-output/js-lex.jisonlex-lexer.js
@@ -0,0 +1,59 @@
+{
+  error: {
+    message: `
+Lexer rule regex action code declaration error?
+    
+  Erroneous code:
+25: "\\"(?:{esc}[\\"bfnrt/{esc}]|{esc}u[a-fA-F0-9]{4}|[^\\"{esc}])*\\"", "yytext = yytext.substr(1,yyleng-2); return 'STRING';"
+^^.................................................................^
+26: 
+27: '/'                             return '/'
+    
+  Technical error report:
+Parse error on line 25:
+"\\"(?:{esc}[\\"bfnrt/{esc}]|{esc}u[a-fA-F0-9]{4}|[^\\"{esc}])*\\"", "yytext ...
+---------------------------------------------------------------^
+Expecting "|", "(", ")", "+", "*", "?", "/", ".", "^", "$", "MACRO_END", "ACTION_START_AT_SOL", "ACTION_START", "ARROW_ACTION_START", "SPECIAL_GROUP", "/!", "REGEX_SPECIAL_CHAR", "ESCAPED_CHAR", macro name in '{...}' curly braces, "REGEX_SET_START", "RANGE_REGEX", "STRING_LIT", "CHARACTER_LIT", "range_regex", got unexpected ","
+`,
+    type: 'JisonParserError',
+    stack: `JisonParserError: 
+Lexer rule regex action code declaration error?
+    
+  Erroneous code:
+25: "\\"(?:{esc}[\\"bfnrt/{esc}]|{esc}u[a-fA-F0-9]{4}|[^\\"{esc}])*\\"", "yytext = yytext.substr(1,yyleng-2); return 'STRING';"
+^^.................................................................^
+26: 
+27: '/'                             return '/'
+    
+  Technical error report:
+Parse error on line 25:
+"\\"(?:{esc}[\\"bfnrt/{esc}]|{esc}u[a-fA-F0-9]{4}|[^\\"{esc}])*\\"", "yytext ...
+---------------------------------------------------------------^
+Expecting "|", "(", ")", "+", "*", "?", "/", ".", "^", "$", "MACRO_END", "ACTION_START_AT_SOL", "ACTION_START", "ARROW_ACTION_START", "SPECIAL_GROUP", "/!", "REGEX_SPECIAL_CHAR", "ESCAPED_CHAR", macro name in '{...}' curly braces, "REGEX_SET_START", "RANGE_REGEX", "STRING_LIT", "CHARACTER_LIT", "range_regex", got unexpected ","
+
+    at Object.parseError (/regexp-lexer-cjs.js:6988:15)
+    at Object.yyError (/regexp-lexer-cjs.js:7171:25)
+    at Object.parser__PerformAction (/regexp-lexer-cjs.js:4034:14)
+    at Object.parse (/regexp-lexer-cjs.js:8235:24)
+    at Object.yyparse [as parse] (/regexp-lexer-cjs.js:12095:25)
+    at autodetectAndConvertToJSONformat (/regexp-lexer-cjs.js:13288:35)
+    at processGrammar (/regexp-lexer-cjs.js:16345:12)
+    at test_me (/regexp-lexer-cjs.js:14269:23)
+    at new RegExpLexer (/regexp-lexer-cjs.js:14387:17)
+    at Context.testEachLexerExample (/regexplexer.js:3685:25)
+    at callFn (/runnable.js:364:21)
+    at Test.Runnable.run (/runnable.js:352:5)
+    at Runner.runTest (/runner.js:677:10)
+    at /runner.js:801:12
+    at next (/runner.js:594:14)
+    at /runner.js:604:7
+    at next (/runner.js:486:14)
+    at cbHookRun (/runner.js:551:7)
+    at done (/runnable.js:308:5)
+    at callFn (/runnable.js:387:7)
+    at Hook.Runnable.run (/runnable.js:352:5)
+    at next (/runner.js:510:10)
+    at Immediate._onImmediate (/runner.js:572:5)
+    at processImmediate (/timers.js:456:21)`,
+  },
+}
